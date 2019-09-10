2751447069ee14ab78719ce5743b9cc175d04425
Angelcho <angelco95@gmail.com>
2019-09-10 19:33:31 -0400

    Initial commit

---
+++ Chat/routing.py
@@ -0,0 +1,13 @@
+# Chat/routing.py
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chatroom.routing
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chatroom.routing.websocket_urlpatterns
+        )
+    ),
+})
---
+++ chatroom/routing.py
@@ -0,0 +1,8 @@
+# chatroom/routing.py
+from django.urls import path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    path('ws/chatroom/<str:room_name>/', consumers.ChatConsumer),
+]
